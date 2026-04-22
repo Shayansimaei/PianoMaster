d524c8c5a63751369d8e6ca365a7c3b64e1a87b6
shayan simaei <shayan78simaei@gmail.com>
2026-04-22 19:50:23 +0100

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
- import { Application } from 'pixi.js';
-import '@pixi/unsafe-eval'; // Import patch before creating application
 
-const app = new Application();
 export default defineConfig({
   plugins: [vue()],
  
